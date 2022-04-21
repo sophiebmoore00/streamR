87b6ca3304a0b1f80f52e5a3e5a3a7a6a9e39ab1
Sophie Moore <sophie.b.moore00@gmail.com>
2022-04-21 13:05:30 -0700

    debugged and tested top_artists()

--- R/streaming_history.R
+++ R/streaming_history.R
@@ -1,36 +1,38 @@
 #' Finds Top Artist you listen to
-#' Finds Top Artist you listen to
 #'
 #' @param filepath JSON file
 #'
-#' @return top playlists
-#'
+#' @return A dataframe with top artists
 #'
 #' @import rjson
 #' @import purrr
-#' @import tidyverse
+#' @import dplyr
+#'
 #' @export
 
+top_artists <- function(filepath, n = 5) {
+
+  dat <- convert_to_df(filepath)
+
+  dat %>%
+    group_by(artistName) %>%
+    count(artistName) %>%
+    rename(count = n) %>%
+    arrange(desc(count)) %>%
+    head(n = n)
+
+}
+
+#' Converts JSON file to a dataframe
+#'
+#' @param filepath The path to the json file
+#'
+#' @return A dataframe
 
 convert_to_df <- function(filepath) {
 
   dat <- fromJSON(file = filepath)
   dat <- purrr::map_dfr(dat, ~.x)
   return(dat)
-
-}
-
-
-top_artists <- function(filepath, n = 5) {
-
-  dat <- counvert_to_df(filepath)
-
-  dat %>%
-    group_by(artistName) %>%
-    count(artistName) %>%
-    mutate(count = n) %>%
-    select(-n) %>%
-    arrange(desc(n)) %>%
-    head(n = n)
 
 }
--- tests/testthat/test-streaming_history.R
+++ tests/testthat/test-streaming_history.R
@@ -1,3 +1,7 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("top artists works", {
+  expect_equal(top_artists("musicmusicmusic/StreamingHistory1.json"))
+})
